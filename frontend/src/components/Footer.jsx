b6764a094e388faf00d943f960f50233c1d7b217
Agenor2512 <noemie.vasseur2512@gmail.com>
2024-03-21 10:02:13 +0100

    Added the navigation bar to footer + mobile style

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,24 @@
 import "../styles/footer.css";
 
+import home from "../assets/home.svg";
+import search from "../assets/search.svg";
+
 function Footer() {
   return (
     <div className="footer-container">
       <button type="button">
         <p>+</p>
       </button>
+      <div>
+        <button type="button">
+          <img src={home} alt="home page button" />
+        </button>
+        <button type="button">
+          <img src={search} alt="search page button" />
+        </button>
+      </div>
     </div>
   );
 }
+
 export default Footer;
