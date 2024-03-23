c247d01051f3acc0152f6fcf61502c9a366a4bdf
Agenor2512 <noemie.vasseur2512@gmail.com>
2024-03-23 16:51:37 +0100

    Added header and footer to search page

--- frontend/src/pages/SearchIngredients.jsx
+++ frontend/src/pages/SearchIngredients.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Search from "../components/Search";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function SearchIngredients() {
   return (
     <div>
+      <Header />
       <Search />
+      <Footer />
     </div>
   );
 }
