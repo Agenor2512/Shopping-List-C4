c1435d73d3d7b20be5fa38ddaa6f449f8c595f79
Agenor2512 <noemie.vasseur2512@gmail.com>
2024-03-23 17:52:00 +0100

    Added get route to router

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,22 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
-
-// Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
+const ingredientControllers = require("./controllers/ingredientControllers");
 
 // Route to get a list of items
-router.get("/items", itemControllers.browse);
-
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
-
-// Route to add a new item
-router.post("/items", itemControllers.add);
-
-/* ************************************************************************* */
+router.get("/ingredients", ingredientControllers.browse);
 
 module.exports = router;
